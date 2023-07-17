26688e665a8f4614c6a2f32dac9ccfb62986aeeb
Tony Kalalian <120026287+tonykalalian@users.noreply.github.com>
2023-07-18 02:53:59 +0300

    Had a problem with my routes and services so I'm putting everything together in app.js and tomorrow I will fix this

--- app.js
+++ app.js
@@ -1,28 +1,93 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const mongoose = require("mongoose");
 require("dotenv").config();
 const app = express();
-const mongoose = require("mongoose");
-const userRoutes = require("./routes/userRoutes");
-
+const PORT = process.env.PORT || 3000;
+mongoose.connect(process.env.MONGODB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String,
+});
+userSchema.plugin(passportLocalMongoose);
+const User = mongoose.model("User", userSchema);
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
-
-mongoose
-  .connect(process.env.MONGODB_URL, { useNewUrlParser: true })
-  .then(() => {
-    console.log("Connected to MongoDB");
+app.use(
+  session({
+    secret: process.env.SECRET || "YourSecretKeyHere",
+    resave: false,
+    saveUninitialized: false,
   })
-  .catch((err) => {
-    console.error("Error connecting to MongoDB:", err);
+);
+app.use(passport.initialize());
+app.use(passport.session());
+app.get("/", (req, res) => {
+  res.render("home");
+});
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+app.get("/secrets", (req, res) => {
+  if (req.isAuthenticated()) {
+    res.render("secrets");
+  } else {
+    res.redirect("/login");
+  }
+});
+app.post("/register", (req, res) => {
+  User.register(
+    { username: req.body.username },
+    req.body.password,
+    (err, user) => {
+      if (err) {
+        console.log(err);
+        res.redirect("/register");
+      } else {
+        passport.authenticate("local")(req, res, () => {
+          res.redirect("/secrets");
+        });
+      }
+    }
+  );
+});
+app.post("/login", (req, res) => {
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password,
   });
-
-// Use routes
-app.use("/", userRoutes);
-
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
+  req.login(user, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      passport.authenticate("local")(req, res, () => {
+        res.redirect("/secrets");
+      });
+    }
+  });
+});
+app.get("/logout", function (req, res) {
+  req.logout(function (err) {
+    if (err) {
+      return err;
+    }
+    res.redirect("/");
+  });
+});
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
 });
